b96e68ef3995327898fa45a94276beec35282d05
Padraic O'Neill <pconeill@gmail.com [core]>
2017-07-11 21:17:16 -0600

    adds email regex validation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,1 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+user = User.create!(first_name: "Viking", last_name: "Raider", phone_number: "999-999-9999", email: "viking.raider@pillage.com", password: "password")
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,7 +7,10 @@ RSpec.describe User, type: :model do
     it {should validate_presence_of(:last_name)}
     it {should validate_presence_of(:phone_number)}
     it {should validate_presence_of(:email)}
-    it {should validate_presence_of(:password_digest)}
+    it {should validate_presence_of(:password)}
+
+    it { should allow_value("email@address.com").for(:email) }
+    it { should_not allow_value("dude").for(:email) }
 
     it {should validate_uniqueness_of(:email)}
     it {should validate_uniqueness_of(:phone_number)}
