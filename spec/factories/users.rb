b726cbfc25077d9b36cd55492be22118d99a4afd
Padraic O'Neill <pconeill@gmail.com [core]>
2017-07-11 20:09:28 -0600

    creates tables for user, roles, and user_roles, creates models and tests

---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :user do
+    birthday "2017-07-11 20:04:56"
+    first_name "MyString"
+    last_name "MyString"
+    phone_number "MyString"
+    email "MyString"
+    password_digest "MyString"
+  end
+end
