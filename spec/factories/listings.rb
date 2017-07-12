a64d27da18c4931278b1f6797c8fd5db5ec29012
Adam Gunther <adamgunther1@gmail.com>
2017-07-12 15:32:53 -0600

    corrects values for enums to match enum text in model

--- spec/factories/listings.rb
+++ spec/factories/listings.rb
@@ -4,6 +4,7 @@ FactoryGirl.define do
     association :cancellation, factory: :cancellation
     association :user, factory: :user
 
+    image_link 'https://a0.muscache.com/im/pictures/21162888/5a0f1151_original.jpg?aki_policy=xx_large'
     name 'Best Place on Earth'
     description 'Place new description here'
     accomodates 3
@@ -12,9 +13,9 @@ FactoryGirl.define do
     beds 6
     price 25.99
     house_rules 'No smoking'
-    property_type 'Apartment'
-    bed_type 'King'
-    pet_type 'Dog(s) And Cat(s)'
-    room_type 'Entire Apt/Home'
+    property_type 'apartment'
+    bed_type 'king'
+    pet_type 'cat_and_dog'
+    room_type 'entire_home'
   end
 end
