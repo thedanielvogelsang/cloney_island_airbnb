e13ec1d483f595ec37eb58fe6e23449a347d816c
William Thomas <wthoma390@gmail.com>
2017-07-12 14:57:06 -0600

    add admin can change property status, admin can log in, guest can search properties specs

---
+++ spec/features/admin_can_log_in_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "Admin can log in" do
+  feature "as an admin I can log in" do
+    admin = create(:user, role: 4)
+
+    visit login_path
+
+    click on "Log In"
+
+    expect(current_path).to eq(new_user_session_path)
+
+    fill_in :user_email, with: "#{admin.email}"
+    fill_in :user_password, with: "#{admin.password}"
+
+    expect(current_path).to eq(root_path)
+    expect(flash[:success]).to match(/You have successfully been logged in!/)
+    expect(page).to have_content("Admin Dashboard")
+    expect(page).to have_content("Logout")
+  end
+end
