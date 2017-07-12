191056dafee3e0d26366fcff1cf0d492d3990a32
Benjamin Ross <benjaminross1001@gmail.com>
2017-07-12 07:05:30 -0600

    reorganizes the flow of user sees messages spec

--- spec/features/user_sees_messages_spec.rb
+++ spec/features/user_sees_messages_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe "On Messages Show Page" do
   end
 
   it "once a traveler requests to book, a conversation begins" do
-    current_user = traveler.id# if forgot how to do this part
+    current_user = traveler.id# i forgot how to do this part, ran out of time to look up
 
     listing = create(:listing, user_id: host.id)
     new_conversation = traveler.conversations.find_by(host_id: host.id)
@@ -21,11 +21,51 @@ RSpec.describe "On Messages Show Page" do
     expect(new_conversation.host_id).to eq(host.id)
   end
 
+  it "hosts can read traveler messages" do
+    listing = create(:listing_with_trip_and_conversation_with_messages, traveler_id: traveler.id, host_id: host.id)#????
+    trip = listing.trips.first
+    conversation = listing.conversations.first
+    latest_message = conversation.messages.last
+
+    visit conversation_path(conversation)
+
+    expect(page).to have_content(latest_message.content)
+    expect(page).to have_content(latest_message.created_at)
+    expect(page).to have_css(".content_message_box")
+  end
+
+  it "hosts can send traveler messages" do
+    current_user = host.id
+
+    listing = create(:listing_with_trip_and_conversation_with_messages, traveler_id: traveler.id, host_id: host.id)#????
+    trip = listing.trips.first
+    conversation = listing.conversations.first
+    latest_message = conversation.messages.last
+
+    visit conversation_path(conversation)
+
+    expect(page).to have_css('.empty_message_box')
+    fill_in "Message", with: "Hey that sounds great!"
+    click_button "Send Message"
+
+    expect(current_path).to eq("/conversations/#{conversation.id}")
+    expect(page).to have_content("Hey that sounds great!")
+    #Somehow test that the host's photo shows up or however we want the conversation designed
+  end
+
   it "once a conversation begins, a host can confirm the reservation" do
+    current_user = host.id
+
     listing = create(:listing_with_trip_and_conversation_with_messages, traveler_id: traveler.id, host_id: host.id)#????
     trip = listing.trips.first
 
-    
+    expect(trip.confirmed?).to eq(false)
+
+    visit conversation_path(trip.conversation)
+
+    click on "Confirm Trip"
+
+    expect(trip.confirmed?).to eq(true)
   end
 
   it "host can view traveler's profile with trip details" do
@@ -55,35 +95,5 @@ RSpec.describe "On Messages Show Page" do
     expect(page).to have_content("Payment")
     expect(page).to have_content("$#{listing.price} x #{trip.duration} nights")
     expect(page).to have_content("$#{trip.total_price}")
-  end
-
-  it "hosts can read traveler messages" do
-    listing = create(:listing_with_trip_and_conversation_with_messages, traveler_id: traveler.id, host_id: host.id)#????
-    trip = listing.trips.first
-    conversation = listing.conversations.first
-    latest_message = conversation.messages.last
-
-    visit conversation_path(conversation)
-
-    expect(page).to have_content(latest_message.content)
-    expect(page).to have_content(latest_message.created_at)
-    expect(page).to have_css(".message_box")
-  end
-
-  it "hosts can send traveler messages" do
-    listing = create(:listing_with_trip_and_conversation_with_messages, traveler_id: traveler.id, host_id: host.id)#????
-    trip = listing.trips.first
-    conversation = listing.conversations.first
-    latest_message = conversation.messages.last
-
-    visit conversation_path(conversation)
-
-    expect(page).to have_css('.empty_message_box')
-    fill_in "Message", with: "Hey that sounds great!"
-    click_button "Send Message"
-
-    expect(current_path).to eq("/conversations/#{conversation.id}")
-    expect(page).to have_content("Hey that sounds great!")
-    #Somehow test that the host's photo shows up or however we want the conversation designed
   end
 end
