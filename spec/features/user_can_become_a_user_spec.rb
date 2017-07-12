fc1542c0f0daf5902269965c8355163c57f8f890
William Thomas <wthoma390@gmail.com>
2017-07-12 15:15:54 -0600

    adds guest can become user spec

---
+++ spec/features/user_can_become_a_user_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe "Guest can become user" do
+  feature "guest can become a user by registering" do
+
+    visit root_path
+
+    click on "Sign Up"
+
+    expect(page).to have_content("Sign up with Facebook")
+    expect(page).to have_content("Sign in with email")
+    click_on "Sign in with email"
+
+    expect(current_path).to eq(new_user_registration_path)
+
+    fill_in :first_name, with: "Bob"
+    fill_in :last_name, with: "Ross"
+    fill_in :email, with: "paint_with_squirrels@bobross.com"
+    fill_in :phone_number, with: "303-867-5309"
+    fill_in :birthday, with: "05-16-1990"
+    fill_in :image_url, with: "https://fakeimage.jpg"
+    fill_in :password, with: "paint"
+    fill_in :password_confirmation, with: "paint"
+    click_on "Sign up"
+
+    expect(current_path).to eq(root_path)
+
+    expect(flash[:alert]).to match(/You have signed up successfully./)
+  end
+end
