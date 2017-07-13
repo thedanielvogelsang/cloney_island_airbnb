cf84ee30236e675c1f4d4f35de44e37b2b611ada
rongxanh88 <rongxanh88@gmail.com>
2017-07-13 12:27:01 -0600

    currently editing rake import file

--- db/seeds.rb
+++
@@ -1,3 +0,0 @@
-user = User.create!(first_name: "Viking", last_name: "Raider", phone_number: "999-999-9999", email: "viking.raider@pillage.com", password: "password")
-
-Role.create!([{name: "traveler"}, {name: "host"}, {name: "admin"}])
