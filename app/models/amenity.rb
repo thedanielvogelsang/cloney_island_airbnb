ddc195fc6ebb1fa90dc8bc106401071beb97521f
adamgunther1 <adamgunther1@gmail.com>
2017-07-12 22:56:07 -0600

    adds validations for address, amenity, role, trip

--- app/models/amenity.rb
+++ app/models/amenity.rb
@@ -1,4 +1,7 @@
 class Amenity < ApplicationRecord
+  validates :name, presence: true
+  validates :name, uniqueness: true
+
   has_many :listing_amenities
   has_many :listings, through: :listing_amenities
 end
