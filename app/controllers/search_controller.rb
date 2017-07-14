f98a763f54f8da67c916aed1eaedf263a67dba60
Benjamin Ross <benjaminross1001@gmail.com>
2017-07-13 20:16:09 -0600

    sql query can find listings based off of city and state

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,5 @@
 class SearchController < ApplicationController
   def index
-    binding.pry
+    listings = Listing.find_by_sql("SELECT listings.id FROM listings JOIN addresses ON listings.address_id = addresses.id WHERE addresses.city LIKE '#{params['city']}' AND addresses.state = '#{params['state']}'")
   end
 end
