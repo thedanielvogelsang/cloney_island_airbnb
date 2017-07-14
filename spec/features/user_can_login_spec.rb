89e52a019c15fe33341230f30f1861c99bc55e1d
Padraic O'Neill <pconeill@gmail.com [core]>
2017-07-13 19:41:14 -0600

    unskips and adds to user_login_spec

--- spec/features/user_can_login_spec.rb
+++ spec/features/user_can_login_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.feature "As a user with an account", type: :feature do
     scenario "user can log in" do
-      skip
       user = create(:user)
       visit login_path
       click_on 'Log In'
@@ -10,7 +9,11 @@ RSpec.feature "As a user with an account", type: :feature do
       expect(current_path).to eq(login_path)
       fill_in "Email", with: user.email
       fill_in "Password", with: user.password
-      click_button 'Sign in'
+      click_button 'Sign In'
       expect(current_path).to eq(users_dashboard_index_path)
+      expect(page).not_to have_content("Sign Up")
+      expect(page).to have_content("Sign Out")
+      expect(page).to have_content("Messages")
+      expect(page).to have_content("Trips")
     end
   end
