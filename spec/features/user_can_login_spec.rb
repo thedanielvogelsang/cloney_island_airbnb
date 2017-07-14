b000cd971c51c31cabed07244e187c48b5b0290f
Padraic O'Neill <pconeill@gmail.com [core]>
2017-07-13 19:51:53 -0600

    unskips user_login, user_logout tests, changes all Login/Logout to Sign In/Sign Out

--- spec/features/user_can_login_spec.rb
+++ spec/features/user_can_login_spec.rb
@@ -4,8 +4,11 @@ RSpec.feature "As a user with an account", type: :feature do
     scenario "user can log in" do
       user = create(:user)
       visit login_path
-      click_on 'Log In'
 
+      within('.navbar-user-types') do
+        click_on 'Sign In'
+      end
+      
       expect(current_path).to eq(login_path)
       fill_in "Email", with: user.email
       fill_in "Password", with: user.password
--- spec/features/user_can_logout_spec.rb
+++ spec/features/user_can_logout_spec.rb
@@ -2,22 +2,26 @@ require 'rails_helper'
 
 RSpec.feature "Logged in user can log out", type: :feature do
   scenario "user sees the visitor options page" do
-    skip
     user = create(:user)
     role = create(:role)
     user.roles << role
-    
+
     visit root_path
 
-    click_on "Sign in"
+    within('.navbar-user-types') do
+      click_on "Sign In"
+    end
+
     fill_in "Email", with: user.email
     fill_in "Password", with: user.password
-    click_on "Sign in"
+    within('.user_forms') do
+      click_on "Sign In"
+    end
 
     #expect(page).to have_content("#{user.username}")
-    click_on "Sign out"
+    click_on "Sign Out"
 
-    expect(page).to have_link "Sign in", href: login_path
-    expect(page).to_not have_content("Sign out")
+    expect(page).to have_link "Sign In", href: login_path
+    expect(page).to_not have_content("Sign Out")
   end
 end
