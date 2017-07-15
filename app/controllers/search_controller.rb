58250d32ab29278f9e30ce8770ec300c3bc24afc
rongxanh88 <rongxanh88@gmail.com>
2017-07-15 11:23:56 -0600

    removed duplicate code

--- app/controllers/search_controller.rb
+++
@@ -1,7 +0,0 @@
-class SearchController < ApplicationController
-  def index
-    binding.pry
-    # @available_listings = Search.find_listings(params)
-    @listings = Listing.search(listing_params)
-  end
-end
