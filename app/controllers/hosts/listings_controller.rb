6f1069d676602d828465e50ef2eca5e7c28debd4
rongxanh88 <rongxanh88@gmail.com>
2017-07-15 15:58:28 -0600

    working on new creationg of listings

--- app/controllers/hosts/listings_controller.rb
+++ app/controllers/hosts/listings_controller.rb
@@ -5,6 +5,38 @@ class Hosts::ListingsController < ApplicationController
   end
 
   def create
+    user = User.find(params[:user_id])
+    address = Address.new(address_params, user_id: user.id)
+    
+    listing = Listing.new(listing_params)
+    # listing.address_id = address.id 
+    listing.user_id = user.id
     binding.pry
+    if listing.save
+      binding.pry
+      redirect_to listing_path(listing)
+    else
+      flash[:message] = "Unable to create new listing"
+      redirect_to new_user_hosts_listing_path(user)
+    end
   end
+
+  private
+
+    def listing_params
+      params.require(:host)
+            .permit(
+              :name, :description, :house_rules, :accomodates,
+              :bathrooms, :bedrooms, :beds, :price, :property_type,
+              :bed_type, :room_type, :pet_type
+            )
+    end
+
+    def address_params
+      params.require(:host).require(:address)
+            .permit(
+              :street_address, :street_address_2, :city, :state, :zip_code
+            )
+    end
+    
 end
