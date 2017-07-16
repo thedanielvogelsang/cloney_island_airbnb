94ad4ec669c3cbe5e199ae1944a085c2c62684de
William Thomas <wthoma390@gmail.com>
2017-07-16 16:38:26 -0600

    adds facebook authorization to application

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,32 +5,35 @@ class SessionsController < ApplicationController
     @user = User.new
   end
   #
-  # def create
-  #   if request.env['omniauth.auth']
-  #     user = User.create_with_omniauth(request.env['omniauth.auth'])
-  #     session[:user_id] = user.id
-  #     redirect_to user_path(user.id)
-  #   else
-  #     user = User.find_by_email(params[:email])
-  #     user && user.authenticate(params[:password])
-  #     session[:user_id] = user.id
-  #     redirect_to user_path(user.id)
-  #   end
-  # end
-
   def create
-    # @user = User.find_by(email: params[:session][:email])
-    auth = request.env['omniauth.auth']
-    @user = User.from_omniauth(auth['provider'], auth['uid'])
-    if @user && @user.authenticate(params[:session][:password])
+    if request.env['omniauth.auth']
+      user = User.from_omniauth(request.env['omniauth.auth'])
+      session[:user_id] = user.id
+      redirect_to user_dashboard_index_path(user)
+    elsif @user && @user.authenticate(params[:session][:password])
+      @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
       flash[:success] = "Logged in as #{@user.first_name}"
       redirect_to user_dashboard_index_path(@user)
     else
       flash[:login_error] = "The email or password you entered is invalid"
-      render :new
+        render :new
     end
   end
+
+  # def create
+  #   # @user = User.find_by(email: params[:session][:email])
+  #   auth = request.env['omniauth.auth']
+  #   @user = User.from_omniauth(request.env["omniauth.auth"])
+  #   if @user && @user.authenticate(params[:session][:password])
+  #     session[:user_id] = @user.id
+  #     flash[:success] = "Logged in as #{@user.first_name}"
+  #     redirect_to user_dashboard_index_path(@user)
+  #   else
+  #     flash[:login_error] = "The email or password you entered is invalid"
+  #     render :new
+  #   end
+  # end
 
   def destroy
     session.clear
--- app/models/user.rb
+++ app/models/user.rb
@@ -30,17 +30,17 @@ class User < ApplicationRecord
   end
 
   def self.from_omniauth(auth)
-      where(provider: auth.provider, uid: auth.uid).first_or_initialize do |user|
+    where(provider: auth.provider, uid: auth.uid).first_or_initialize do |user|
       user.provider = auth.provider
       user.uid = auth.uid
       user.first_name = auth.info.name
       user.last_name = auth.info.name
-      user.phone_number = auth.phone_number
-      user.birthday = auth.birthday
+      user.email = auth.info.email
+      user.phone_number = "505-234-9092"
+      user.birthday = Date.new
       user.password = SecureRandom.urlsafe_base64
       user.oauth_token = auth.credentials.token
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
-      binding.pry
       user.save!
     end
   end
