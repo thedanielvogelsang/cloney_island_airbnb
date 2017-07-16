2a591eb770af620587986da0a279cd66d544e90d
rongxanh88 <rongxanh88@gmail.com>
2017-07-16 15:14:43 -0600

    add search functionality for num guests

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,6 +1,8 @@
 class ListingsController < ApplicationController
   def index
-    if params[:search_address] != ""
+    if params[:search_address] != "" && params[:search_num_guests] != ""
+      @listings = Listing.search_address_and_num_guests(params)
+    elsif
       @listings = Listing.search_address(params[:search_address])
     else
       flash[:message] = "Unable to find listings related to your search."
--- spec/factories/listing_images.rb
+++ spec/factories/listing_images.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :listing_image do
     listing
-
+    
     property_image { File.new("#{Rails.root}/lib/assets/baby_penguin.jpg") }
   end
 end
