e705621fbb7190b381a7e65f08cdf9831b4333ef
Padraic O'Neill <pconeill@gmail.com [core]>
2017-07-18 14:35:41 -0600

    finishes writing tests for experiences CRUD

--- spec/features/experiences/authenticated_user_can_view_experiences_spec.rb
+++ spec/features/experiences/authenticated_user_can_view_experiences_spec.rb
@@ -3,43 +3,159 @@ require 'rails_helper'
 RSpec.describe 'Authenticated user visits /experiences' do
 
   xit 'can view all experiences' do
-    #when I visit root
-    # and I click on 'Experiences' within navbar-header
-    # I arrive to /experiences
-    # I see a list of experiences within container-main
-    # I see experience image within experience-card
-    #I see experience price
-    #I see experience title
-    #When I hover over the image it zoooms
 
+    #create 4 experiences
+    visit root_path
+    within('navbar-header') do
+      click_on 'Experiences'
+    end
 
-  end
+    expect(path).to eq('/experiences')
 
-  xit 'can view a single experience' do
-    #when I visit /experiences
-    #and I click on an experience within css
-    #I arrive to the experience show page
-    #and I see within title block, city, brief description
-    #and within category block I see the word "Category", "Hosted by: [host_name]", the host's profile picture, experience_duration, experience provisions
-    #And on the right I see an experience image, with $price 'per person', date,  and social sharing links
-    #And under Title and Category blocks I'll see the "What we'll do" block with a brief description
-    #And under that I'll see the "What I provide" block with a description
-    #and under that I'll see a "Notes" block with description
-    #and under that I'll see a "Where we'll be" block with a description and a map with the location pinned.
-    #and under that I'll see a about your host block with words "About your host [host_name]" and a personal description.
-    #and under that I'll see a contact link to the host's messaging system
-    #and under that I'll see a list of available dates for this experience
-    #and then I'll see Reviews
-    #and then I'll see Group Size, Guest Requirements, and Cancellation Policy Type
-  end
+    within('.experiences-box') do
+      expect(page).to have_content(experience1.title)
+      expect(page).to have_content(experience2.title)
+      expect(page).to have_content(experience3.title)
+      expect(page).to have_content(experience4.title)
 
-  context 'the experience belongs_to the current_user' do
+      expect(page).to have_content(experience1.image)
+      expect(page).to have_content(experience2.image)
+      expect(page).to have_content(experience3.image)
+      expect(page).to have_content(experience4.image)
 
-    xit 'sees an Edit link' do
+      expect(page).to have_content(experience1.price)
+      expect(page).to have_content(experience2.price)
+      expect(page).to have_content(experience3.price)
+      expect(page).to have_content(experience4.price)
 
+      expect(page).to have_css('experiences-box img:hover')
     end
 
   end
 
+  context 'views a single experience' do
+    visit experiences_path
 
+    within('.experiences-box') do
+      click_on ('img')
+    end
+
+    expect(path).to eq("/experience/#{experience.id}")
+
+    xit 'can see experience title block' do
+      within('.experience-box .title') do
+        expect(page).to have_content(experience.title)
+        expect(page).to have_content(experience.city)
+        expect(page).to have_content(experience.description)
+      end
+    end
+
+    xit 'can see experience category block' do
+      within('.experience-box .category') do
+        expect(page).to have_content(experience.category)
+        expect(page).to have_content("Hosted by: #{experience.host.name}")
+        expect(page).to have_css('.host-profile-pic img')
+        expect(page).to have_content(experience.duration)
+        expect(page).to have_content(experience.provisions)
+      end
+    end
+
+    xit 'can see experience featured image' do
+      within('experience-feature-image') do
+        expect(page).to have_css('img')
+        expect(page).to have_content("$#{experience.price} per person")
+        expect(page).to have_content('See Dates')
+        expect(page).to have_css('.social-sharing .facebook')
+        expect(page).to have_css('.social-sharing .twitter')
+      end
+    end
+
+    xit 'can see experience what block' do
+      within('experience-what') do
+        expect(page).to have_content("What we'll do")
+        expect(page).to have_content(experience.what)
+      end
+    end
+
+    xit 'can see experience provisions block' do
+      within('experience-provisions') do
+        expect(page).to have_content("What I'll provide")
+        expect(page).to have_content(experience.provisions)
+      end
+    end
+
+    xit 'can see experience notes block' do
+      within('experience-notes') do
+        expect(page).to have_content('Notes')
+        expect(page).to have_content(experience.notes)
+      end
+    end
+
+    xit 'can see experience where block' do
+      within('experience-where') do
+        expect(page).to have_content("Where we'll be")
+        expect(page).to have_content(experience.where)
+      end
+    end
+
+    xit 'can see experience host details block' do
+      within('experience-host') do
+        expect(page).to have_content("About your host, #{experience.hosts.name}")
+        expect(page).to have_content(experience.hosts.description)
+      end
+    end
+
+    xit 'can see experience host contact link' do
+      within('experience-host-contact') do
+        expect(page).to have_content('Contact Host') #links to messaging system
+      end
+    end
+
+    xit 'can see experience availability dates list' do
+      within('experience-availability-dates') do
+        expect(page).to have_content('something equaling the availabl dates for this experience')
+      end
+    end
+
+    xit 'can see experience extras block' do
+      within('experience-extra') do
+        expect(page).to have_conent("Group Size: #{experience.group_size}")
+        expect(page).to have_content("Guest Requirements: #{experience.guest_requirements}")
+        expect(page).to have_content("Cancellation Policy Type: #{experience.cancellation_policy}")
+      end
+    end
+
+  end
+
+  context 'the experience belongs_to the current_user' do
+    #user must be host
+    xit 'sees an Edit link' do
+      within('.host-edit-link') do
+        expect(page).to have_link('Edit your experience')
+        click_on "Edit your experience"
+      end
+
+      expect(path).to eq(edit_experience_path(experience.id))
+    end
+  end
+
+  context 'the experience does not belong to the host' do
+    xit 'cannot see an Edit your experience link' do
+      expect(page).not_to have_link('Edit your experience')
+    end
+  end
+
+  context 'as a traveler' do
+    xit 'cannot see an Edit your experience link' do
+      #create traveler user
+      expect(page).not_to have_link('Edit your experience')
+    end
+  end
+
+  context 'as a guest' do
+    xit 'cannot see an Edit your experience link' do
+      #create non-host user
+      expect(page).not_to have_link('Edit your experience')
+    end
+  end
 end
--- spec/features/experiences/traveler_can_create_an_experience_and_become_a_host_spec.rb
+++ spec/features/experiences/traveler_can_create_an_experience_and_become_a_host_spec.rb
@@ -10,7 +10,20 @@ RSpec.describe 'As a traveler' do
       click_on 'Host an Experience'
 
       expect(path).to be(new_experience_path)
-      expect(page).to have_css("input#post_title", :value => "Category")
+      expect(page).to have_field("Category", :value => "Category") #Check boxes
+      expect(page).to have_field("Title", :value => "Title") #Text field
+      expect(page).to have_field("Time", :value => "Time") #Text field
+      expect(page).to have_field("Tagline", :value => "Tagline") #Text box
+      expect(page).to have_field("Photos", :value => "Photos") # Photo upload
+      expect(page).to have_field("What", :value => "What We'll Do") #Text box
+      expect(page).to have_field("Where", :value => "Where We'll Meet") #Text box
+      expect(page).to have_field("Provisions", :value => "What I'll Provide") #Text box
+      expect(page).to have_field("Notes", :value => "Notes") #Text box
+
+      click_on "Create Experience"
+
+      expect(user.roles).should include('traveler') #make this work!
+      expect(user.roles).should include('host') # and this!
     end
   end
 end
