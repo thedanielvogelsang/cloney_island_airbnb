78f55f79f621204b6662bd318b1059bf32dc2af3
Padraic O'Neill <pconeill@gmail.com [core]>
2017-07-18 08:56:44 -0600

    creates experiences test files and adds some tests

---
+++ spec/features/experiences/guest_can_view_experiences_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe 'Guest visits /experiences' do
+
+  xit 'can view all experiences' do
+    #when I visit root
+    # and I click on 'Experiences' within navbar-header
+    # I arrive to /experiences
+    # I see a list of experiences within container-main
+    # I see experience image within experience-card
+    #I see experience price
+    #I see experience title
+    #When I hover over the image it zoooms
+
+
+  end
+
+  xit 'can view a single experience' do
+    #when I visit /experiences
+    #and I click on an experience within css
+    #I arrive to the experience show page
+    #and I see within title block, city, brief description
+    #and within category block I see the word "Category", "Hosted by: [host_name]", the host's profile picture, experience_duration, experience provisions
+    #And on the right I see an experience image, with $price 'per person', date,  and social sharing links
+    #And under Title and Category blocks I'll see the "What we'll do" block with a brief description
+    #And under that I'll see the "What I provide" block with a description
+    #and under that I'll see a "Notes" block with description
+    #and under that I'll see a "Where we'll be" block with a description and a map with the location pinned.
+    #and under that I'll see a about your host block with words "About your host [host_name]" and a personal description.
+    #and under that I'll see a contact link to the host's messaging system
+    #and under that I'll see a list of available dates for this experience
+    #and then I'll see Reviews
+    #and then I'll see Group Size, Guest Requirements, and Cancellation Policy Type
+  end
+
+end
