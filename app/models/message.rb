def033e9552f27d807a4ee0f581f3b261f47cecd
adamgunther1 <adamgunther1@gmail.com>
2017-07-19 16:53:50 -0600

    adds to message model that it belongs to a conversation

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,7 @@
 class Message < ApplicationRecord
   validates :content, presence: true
   
-  # belongs_to :conversation
+  belongs_to :conversation
   belongs_to :user
 
   scope :for_display, -> { order(:created_at).last(50) }
--- spec/features/experiences/host_can_create_create_an_experience_spec.rb
+++ spec/features/experiences/host_can_create_create_an_experience_spec.rb
@@ -8,15 +8,15 @@ RSpec.describe 'As a host' do
     click_on 'Host an Experience'
 
     expect(path).to be(new_experience_path)
-    fill_in "Category", :with => "Arts") #Check boxes
-    fill_in "Title", :with => "Visit the Only Worthy Gallery in the Galaxy") #Text field
-    fill_in "Time", :with => "2 hours") #Text field
-    fill_in "Tagline", :with => "If you're with us, you're awesome!") #Text box
-    #fill_in "Photos", :with => "Photos") # Photo upload
-    fill_in "What", :with => "Walk around and ask questions about art. Do you buy art because it's evocative? Would you buy art that disgusts you becaues it evokes that feeling?") #Text box
-    fill_in "Where", :with => "The place beyond the pines.") #Text box
-    fill_in "Provisions", :with => "Water, energy bars, chewing gum.") #Text box
-    fill_in "Notes", :with => "Bring yourself. Bring your smile. Bring your happiness and share it.") #Text box
+    fill_in "Category", :with => "Arts" #Check boxes
+    fill_in "Title", :with => "Visit the Only Worthy Gallery in the Galaxy" #Text field
+    fill_in "Time", :with => "2 hours" #Text field
+    fill_in "Tagline", :with => "If you're with us, you're awesome!" #Text box
+    #fill_in "Photos", :with => "Photos" # Photo upload
+    fill_in "What", :with => "Walk around and ask questions about art. Do you buy art because it's evocative? Would you buy art that disgusts you becaues it evokes that feeling?" #Text box
+    fill_in "Where", :with => "The place beyond the pines." #Text box
+    fill_in "Provisions", :with => "Water, energy bars, chewing gum." #Text box
+    fill_in "Notes", :with => "Bring yourself. Bring your smile. Bring your happiness and share it." #Text box
 
     click_on "Create Experience"
 
