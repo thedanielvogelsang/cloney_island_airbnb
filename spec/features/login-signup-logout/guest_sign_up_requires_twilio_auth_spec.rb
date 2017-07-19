721cf8683e44c06fe4227ee09dd1600a6fe97909
Benjamin Ross <benjaminross1001@gmail.com>
2017-07-18 20:07:57 -0600

    passes two more tests

--- spec/features/login-signup-logout/guest_sign_up_requires_twilio_auth_spec.rb
+++ spec/features/login-signup-logout/guest_sign_up_requires_twilio_auth_spec.rb
@@ -1,52 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe "Two Factor Authentication" do
-  context "allows users to reset a forgotten password" do
+  context "allows users to verify their phone" do
     let(:fake_verification_code) { "123456" }
 
-    xit "when they are logged in" do
+    xit "when they enter their code" do
       allow(CodeGenerator)
         .to receive(:generate)
         .and_return(fake_verification_code)
 
-      traveler = create(:user)
+      traveler = create(:user, verification_code: fake_verification_code)
 
       allow_any_instance_of(ApplicationController)
       .to receive(:current_user)
       .and_return(traveler)
+      current_user = traveler
 
-      visit edit_user_path
-
-      VCR.use_cassette("twilio-accounts-messages") do
-        click_on "Change Password"
-      end
+      visit ("/confirmations/new/")
 
       expect(page).to have_content("Verification Code")
 
       expect(page).to have_button("Confirm")
 
       fill_in "verification_code", with: fake_verification_code
-      click_on "Confirm"
-
-      expect(page).to have_content("Verification code is correct!")
-    end
-
-    xit "when they are logged out" do
-      allow(CodeGenerator)
-      .to receive(:generate)
-      .and_return(fake_verification_code)
-
-      visit root_path
-
-      click_on "Sign In"
-      expect(current_path).to eq(login_path)
-
-      expect(page).to have_css('#reset-email-field', visible: false)
-      click_on "Reset Password"
-      expect(page).to have_css('#reset-email-field', visible: true)
-
-      fill_in "reset-email-field", with: "#{user.email}"
-      click_on "Submit"
+      click_on "Check Validation Code"
 
       expect(page).to have_content("Verification code is correct!")
     end
