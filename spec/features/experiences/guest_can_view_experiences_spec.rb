ab3cdbf5fddf7410b0c0f61ca35e004b4d49a823
Padraic O'Neill <pconeill@gmail.com [core]>
2017-07-19 09:19:33 -0600

    reduces scope of experience show page, passing show page spec

--- spec/features/experiences/guest_can_view_experiences_spec.rb
+++ spec/features/experiences/guest_can_view_experiences_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Guest visits /experiences' do
 
-  xit 'can view all experiences' do
+  it 'can view all experiences' do
     experiences = create_list(:experience, 4)
     experience = experiences.first
     #image = create(:experience_image)
@@ -37,9 +37,7 @@ RSpec.describe 'Guest visits /experiences' do
     visit experiences_path
 
     find(:xpath, "//a[@href='/experiences/#{id}']").click
-
     expect(current_path).to eq("/experiences/#{id}")
-
 
     within('.experience-title-box') do
       expect(page).to have_content(experience.title)
@@ -50,89 +48,78 @@ RSpec.describe 'Guest visits /experiences' do
     within('.experience-category-box') do
       #expect(page).to have_content(experience.experience_categories.first)
       #expect(page).to have_content(experience.experience_categories.last)
-      expect(page).to have_content("Hosted by: #{experience.host.name}")
+      expect(page).to have_content("Hosted by: #{experience.user.full_name}")
       #expect(page).to have_image('small host profile pic')
       expect(page).to have_content(experience.duration)
       expect(page).to have_content(experience.provisions)
     end
 
-    xit 'can see featured image block' do
-      within('.experience-featured-image') do
-        #expect(page).to have the featured image
-        expect(page).to have_content("#{experience.price} per person")
-        expect(page).to have_content("See Dates")
-      end
+    within('.experience-featured-image') do
+      #expect(page).to have the featured image
+      expect(page).to have_content("#{experience.price} per person")
+      #expect(page).to have_content("See Dates")
     end
 
-    xit 'can view all dates' do
-      within('.experience-date') do
-        click_on 'See Dates'
-        #expect a popup to pop up
-        expect(page).to have_content(experience.dates.first)
-        expect(page).to have_content(experience.dates.last)
-        expect(page).to have_content("Book Now", count: 4)
-      end
+    # within('.experience-date') do
+    #   click_on 'See Dates'
+    #   #expect a popup to pop up
+    #   expect(page).to have_content(experience.dates.first)
+    #   expect(page).to have_content(experience.dates.last)
+    #   expect(page).to have_content("Book Now", count: 4)
+    # end
+
+    within('.experience-what-box') do
+      expect(page).to have_content("What We'll Do")
+      expect(page).to have_content(experience.what)
     end
 
-    xit 'can see the what we will do block' do
-      within('.experience-what-box') do
-        expect(page).to have_content("What We'll Do")
-        expect(page).to have_content(experience.what)
-      end
-    end
-
-    xit 'can see what the host will provide' do
+    within('.experience-host-provides-box') do
       expect(page).to have_content("What I'll Provide")
       expect(page).to have_content(experience.provisions)
     end
 
-    xit 'can see notes on the experience' do
+    within('.experience-notes-box') do
       expect(page).to have_content('Notes')
       expect(page).to have_content(experience.notes)
     end
 
-    xit 'can see where the experience will be on a map' do
+    within('.experience-where-box') do
       expect(page).to have_content("Where We'll Be")
+      expect(page).to have_content(experience.where)
       expect(page).to have_css('.experience-map')
       #how to test for map accuracy?
     end
 
-    xit 'can see a brief host description' do
-      within('.host-block') do
-        expect(page).to have_content("About your host, #{experience.host.name}")
-        #this should be unique for each experience so the host can brand himself/herself according to the experience
-        expect(page).to have_content(experience.host_description)
-      end
+    within('.host-block') do
+      expect(page).to have_content("About your host, #{experience.user.first_name}")
+      #this should be unique for each experience so the host can brand himself/herself according to the experience
+      expect(page).to have_content(experience.host_description)
     end
 
-    xit 'can see a link to message the host' do
-      within('.host-contact') do
-        expect(page).to have_link('Contact Host')
-      end
-    end
+    # within('.host-contact') do
+    #   expect(page).to have_link('Contact Host')
+    # end
 
-    xit 'can view available dates' do
-      within('.available-dates') do
-        expect(page).to have_link("See All Dates")
-      end
-    end
+    # xit 'can view available dates' do
+    #   within('.available-dates') do
+    #     expect(page).to have_link("See All Dates")
+    #   end
+    # end
+    #
+    # xit 'can see reviews for the experience' do
+    #   within('.experience-reviews') do
+    #     expect(page).to have_content('Reviews')
+    #     expect(page).to have_content(experience.reviews.first.content)
+    #     expect(page).to have_content(experience.reviews.first.title)
+    #     expect(page).to have_content(experience.reviews.first.name)
+    #   end
+    # end
 
-    xit 'can see reviews for the experience' do
-      within('.experience-reviews') do
-        expect(page).to have_content('Reviews')
-        expect(page).to have_content(experience.reviews.first.content)
-        expect(page).to have_content(experience.reviews.first.title)
-        expect(page).to have_content(experience.reviews.first.name)
-      end
-    end
-
-    xit 'can see the extra info about the experience' do
+    within('.experience-extras') do
       expect(page).to have_content("Extra Info")
       expect(page).to have_content(experience.group_size)
       expect(page).to have_content(experience.guest_requirements)
       expect(page).to have_content(experience.cancellation_policy_type)
     end
-
   end
-
 end
