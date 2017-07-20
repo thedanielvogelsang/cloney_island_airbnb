d0ecc0e70c70aaa2d8c2545a6ac5133ceb2c5f09
Padraic O'Neill <pconeill@gmail.com [core]>
2017-07-20 01:04:29 -0600

    passes guest must create account to create experience spec

--- spec/features/experiences/guest_must_create_account_create_experience_spec.rb
+++ spec/features/experiences/guest_must_create_account_create_experience_spec.rb
@@ -1,19 +1,13 @@
 require 'rails_helper'
+require_relative 'experience_helper'
 
 RSpec.describe 'As a guest' do
+  include ExperienceHelper
 
-  xit 'must create an account to book an experience' do
-    visit experiences_path
+  it 'must create an account to create an experience' do
+    visit new_experience_path
 
-    within.first('experience-box') do
-      click_on 'the image'
-    end
-
-    expect(path).to be('/experience/:id')
-    click_on "Book Now"
-    expect('popup').to have_content("You need to sign in or create an account to Book this Experience")
-    expect('popup').to have_link("Sign In")
-    expect('popup').to have_link("Create an Account")
+    expect(current_path).to eq(login_path)
   end
 
 end
