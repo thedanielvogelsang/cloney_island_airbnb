6dce7d612db1e8773d0af4ccfdd9c1293cc6fa6d
Jim Szalewski <jim.szalewski@gmail.com>
2017-09-15 10:12:54 -0600

    fix merge conflicts, detailed descriptions will be in PR

--- spec/features/login-signup-logout/guest_sign_up_requires_twilio_auth_spec.rb
+++ spec/features/login-signup-logout/guest_sign_up_requires_twilio_auth_spec.rb
@@ -20,7 +20,29 @@ RSpec.describe "Two Factor Authentication" do
       fill_in 'confirmations[verification_code]', with: fake_verification_code
       click_on "Check Validation Code"
 
+      expect(current_path).to eq(user_dashboard_index_path(traveler))
       expect(page).to have_content("Welcome #{traveler.first_name}")
+    end
+  end
+
+  context 'user enters invalid verification code' do
+    let(:fake_verification_code) { "123456" }
+    it 'redirects to verification page and displays an error message' do
+      allow(CodeGenerator)
+        .to receive(:generate)
+        .and_return(fake_verification_code)
+
+      traveler = create(:user, verification_code: fake_verification_code)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(traveler)
+
+      visit ("/confirmations/new")
+
+      fill_in 'confirmations[verification_code]', with: "999999"
+      click_on "Check Validation Code"
+
+      expect(current_path).to eq(confirmations_path)
+      expect(page).to have_content("Verification code is incorrect.")
     end
   end
 end
