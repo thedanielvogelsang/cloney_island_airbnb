8d473135a2988472989984251029b6c9b356db4e
Chantal Justamond <cjustamm66@me.com>
2017-09-17 12:33:17 -0600

    picture_url added to be able to display property image

--- app/models/airbnb.rb
+++ app/models/airbnb.rb
@@ -3,12 +3,14 @@ class Airbnb
   attr_reader :name,
               :beds,
               :room_type,
-              :price
+              :price,
+              :property_image
 
   def initialize(search)
-    @name      = search[:listing][:name]
-    @beds      = search[:listing][:beds]
-    @room_type = search[:room_type_category]
-    @price     = search[:pricing_quote][:rate][:amount]
+    @name           = search[:listing][:name]
+    @beds           = search[:listing][:beds]
+    @room_type      = search[:room_type_category]
+    @price          = search[:pricing_quote][:rate][:amount]
+    @property_image = search[:listing][:picture_url]
   end
 end
