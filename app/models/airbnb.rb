1dfe4bcd9a4bc11395326c8c7381640979d2a88f
Chantal Justamond <cjustamm66@me.com>
2017-09-17 12:40:14 -0600

    displaying dynamic info :hatching_chick:

--- app/models/airbnb.rb
+++ app/models/airbnb.rb
@@ -4,13 +4,13 @@ class Airbnb
               :beds,
               :room_type,
               :price,
-              :property_image
+              :listing_image
 
   def initialize(search)
     @name           = search[:listing][:name]
     @beds           = search[:listing][:beds]
     @room_type      = search[:room_type_category]
     @price          = search[:pricing_quote][:rate][:amount]
-    @property_image = search[:listing][:picture_url]
+    @listing_image = search[:listing][:picture_url]
   end
 end
