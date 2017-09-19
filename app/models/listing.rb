563821d18af3622185b3ceceb695e9833c64b074
thedanielvogelsang <dr.vogelsang26@gmail.com>
2017-09-19 15:15:29 -0600

    removes almost everything from listing.rb

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,28 +1,4 @@
 class Listing < ApplicationRecord
-
   validates :airbnb_id
-  
   has_many :trips
-
-  enum property_type: [:house, :apartment, :guesthouse, :boat, :treehouse]
-  enum room_type: [:entire_home, :private_room, :shared_room]
-  enum bed_type: [:king, :queen, :double, :twin, :single, :couch]
-  enum pet_type: [:no_pets, :cat, :dog, :cat_and_dog, :misc]
-  enum status: [:unlisted, :listed]
-  enum cancellation_policy: [:flexible, :moderate, :strict]
-
-  accepts_nested_attributes_for :listing_images, :reject_if => lambda { |t| t['listing_image'].nil? }
-
-  scope :listed, -> { where(status: 1) }
-
-  def self.search_address(address_param)
-    address = address_param.downcase
-    where("LOWER(listings.address) LIKE ?", "%#{address}%").listed
-  end
-
-  def self.search_address_and_num_guests(params)
-    address = params[:search_address].downcase
-    num_guests = params[:search_num_guests].to_i
-    where("LOWER(listings.address) LIKE ? AND listings.accomodates >= ?", "%#{address}%", num_guests).listed
-  end
 end
